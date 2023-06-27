93c8ebdaa5c5aec6b627846af6a01e1d24507aa9
osvetilka <defunct@inbox.ru>
2023-06-28 02:54:53 +0500

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import React, {Component} from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">     
+      <Header/>
+    </div> 
+   /* <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./logo" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +22,107 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
+
+// заголовок
+class Header extends Component {
+  render () {
+    return (
+      <header className="menu-header" >
+      <Logo/>                
+        Здесь должно быть меню
+      </header>
+      /*<div className="header">
+        <Logo className="App-logo"/>
+        <Menu className="header-menu"/>
+      </div>*/
+      );
+  }
+}
+
+// логотип сайта
+class Logo extends Component {
+  render() {
+    return (
+      <a href="index.js"><img src={logo} alt="Прогноз погоды"/></a>
+    )   
+  }
+}
+
+// главное меню
+/*class MainMenu extends Component {
+  render() {
+    return (
+    
+  }
+}*/
+
+// пункты меню заголовка
+/*class HeaderMenu extends Component {
+  render () {
+    return
+      
+  }
+}
+
+// поле поиска
+class HeaderMenu extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+// Меню периода
+class PeriodMenu extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+// Контейнер для оснвной части прогноза погоды
+class WeatherContainer extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+// Контейнер для рекламы
+class AdsContainer extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+// полный путь населённого пункта
+class TownFullPath extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+// заголовок периода
+class PeriodHeader extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+// заголовок периода
+class PeriodHeader extends Component {
+  render () {
+    return
+ 
+  }
+}
+
+*/
 
 export default App;
